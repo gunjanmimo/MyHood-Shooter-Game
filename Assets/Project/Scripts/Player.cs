0ff24b2be638a504e53a3dd7cfa560f9a5fae0b8
gunjanpaul <gunjan.mtbpaul@gmail.com>
2020-12-15 09:38:17 +0530

    new

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -7,12 +7,15 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("fire");
+        }
     }
 }
